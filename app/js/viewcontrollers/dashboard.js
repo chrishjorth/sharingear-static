1e353184baad08a957963e4fc02606fc439cf4af
Chris Hjorth <contact@chrishjorth.com>
2014-08-19 15:53:14 +0200

    Added didRender call to Dashboard.

--- app/js/viewcontrollers/dashboard.js
+++ app/js/viewcontrollers/dashboard.js
@@ -4,9 +4,18 @@
  */
 
 define(
-	['viewcontroller'],
-	function(ViewController) {
-		var Dashboard = ViewController; 
+	['underscore', 'viewcontroller'],
+	function(_, ViewController) {
+		var Dashboard = ViewController;
+
+		_.extend(Dashboard.prototype, {
+			didRender: didRender
+		});
+
 		return Dashboard;
+
+		function didRender() {
+			console.log('Dashboard did render with path: ' + this.path);
+		}
 	}
 );
