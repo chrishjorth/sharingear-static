7a3be6398398a26a6735735e019946b315726f93
Chris Hjorth <contact@chrishjorth.com>
2014-08-25 21:22:39 +0200

    Added profile to dashboard

--- app/js/viewcontrollers/dashboard.js
+++ app/js/viewcontrollers/dashboard.js
@@ -12,7 +12,9 @@ define(
 			subPath: '',
 			didInitialize: didInitialize,
 			didRender: didRender,
-			loadSubView: loadSubView
+			loadSubView: loadSubView,
+			getSubviewParameters: getSubviewParameters,
+			getProfileParameters: getProfileParameters
 		});
 
 		return Dashboard;
@@ -33,11 +35,31 @@ define(
 			var router = this;
 			require(['text!../templates/dashboard-' + router.subPath + '.html'], function(SubViewTemplate) {
 				var template = _.template(SubViewTemplate);
-				router.$subViewContainer.html(template());
+				router.$subViewContainer.html(template(router.getSubviewParameters(router.subPath)));
 				if(callback && typeof callback === 'function') {
 					callback();
 				}
 			});
+		}
+
+		function getSubviewParameters(subView) {
+			var parameters = {};
+			switch(subView) {
+				case 'profile':
+					parameters = this.getProfileParameters();
+					break;
+			}
+			return parameters;
+		}
+
+		function getProfileParameters() {
+			var profileParameters = {
+				name: 'Chris Hjorth',
+				hometown: 'Aalborg',
+				bio: 'Blah blah',
+				genres: ''
+			};
+			return profileParameters;
 		}
 	}
 );
