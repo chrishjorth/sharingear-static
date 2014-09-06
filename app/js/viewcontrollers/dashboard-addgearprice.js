449e04e5611ba84bc2170a181fe01dde7ed2091e
Chris Hjorth <contact@chrishjorth.com>
2014-09-06 12:32:08 +0200

    Added add gear price template dummy with controller.

---
+++ app/js/viewcontrollers/dashboard-addgearprice.js
@@ -0,0 +1,13 @@
+/**
+ * Controller for the Sharingear Add gear pricing dashboard page view.
+ * @author: Chris Hjorth
+ */
+
+define(
+	['utilities', 'viewcontroller'],
+	function(Utilities, ViewController) {
+		var AddGearPrice = Utilities.inherit(ViewController, {
+		}); 
+		return AddGearPrice;
+	}
+);
