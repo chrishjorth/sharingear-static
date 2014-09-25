351642954ee95a435789e9646e03be5699aa97dc
Chris Hjorth <contact@chrishjorth.com>
2014-09-25 16:03:32 +0200

    Live debugging.

--- app/js/viewcontrollers/home.js
+++ app/js/viewcontrollers/home.js
@@ -53,6 +53,7 @@ define(
 				var locationData;
 				if(status === GoogleMaps.GeocoderStatus.OK) {
 					locationData = results[0].geometry.location.lat() + ',' + results[0].geometry.location.lng();
+					console.log(locationData);
 					view.gearList.search(locationData, $('#home-search-form #search-gear', this.$element).val(), '20140828-20140901', function(searchResults) {
 						view.populateSearchBlock(searchResults);
 						if(callback && typeof callback === 'function') {
