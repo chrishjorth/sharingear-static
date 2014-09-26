98f668bdf4d2eafe012b227d855a88140e2ce29f
Chris Hjorth <contact@chrishjorth.com>
2014-09-26 11:06:06 +0200

    Added month/week switch to dashboard calendar.

--- app/js/viewcontrollers/dashboard-calendar.js
+++ app/js/viewcontrollers/dashboard-calendar.js
@@ -7,7 +7,47 @@ define(
 	['viewcontroller'],
 	function(ViewController) {
 		var Calendar = ViewController.inherit({
+			weekMode: true, //if false then it is months mode
+
+			didRender: didRender,
+
+			switchToWeeks: switchToWeeks,
+			switchToMonths: switchToMonths
 		});
+
 		return Calendar;
+
+		function didRender() {
+			this.setupEvent('click', '#dashboard-calendar-weeks-btn', this, this.switchToWeeks);
+			this.setupEvent('click', '#dashboard-calendar-months-btn', this, this.switchToMonths);
+		}
+
+		function switchToWeeks(event) {
+			var view = event.data,
+				$monthsContainer;
+			if(view.weekMode === true) {
+				return;
+			}
+			$monthsContainer = $('#calendar-months-container', view.$element);
+			if($monthsContainer.hasClass('hidden') === false) {
+				$monthsContainer.addClass('hidden');
+			}
+			$('#calendar-weeks-container', view.$element).removeClass('hidden');
+			view.weekMode = true;
+		}
+
+		function switchToMonths(event) {
+			var view = event.data,
+				$weeksContainer;
+			if(view.weekMode === false) {
+				return;
+			}
+			$weeksContainer = $('#calendar-weeks-container', view.$element);
+			if($weeksContainer.hasClass('hidden') === false) {
+				$weeksContainer.addClass('hidden');
+			}
+			$('#calendar-months-container', view.$element).removeClass('hidden');
+			view.weekMode = false;
+		}
 	}
 );
