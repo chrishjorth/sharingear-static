e193ac25e640e4bb958fc5200cbc9fae5ed7554d
Chris Hjorth <contact@chrishjorth.com>
2014-10-10 15:04:15 +0200

    Fixed issue with profile image not loading in dashboard.

--- app/js/viewcontrollers/dashboard-profile.js
+++ app/js/viewcontrollers/dashboard-profile.js
@@ -7,51 +7,58 @@ define(
 	['underscore', 'viewcontroller', 'app'],
 	function(_, ViewController, App) {
 		var Profile = ViewController.inherit({
+            user: null,
+            profileImg: null,
+            profileImgLoaded: $.Deferred(),
+            renderResolve: $.Deferred(),
+
 			didInitialize: didInitialize,
             handleImageUpload: handleImageUpload,
             didRender: didRender,
             handleSave:handleSave,
-
-            user: null
 		}); 
 		return Profile;
 
-        function didRender() {
-            this.setupEvent('change','#profile-pic',this,this.handleImageUpload);
-        }
-		function didInitialize() {
-			//We need default values so the templating does not fail.
-			var user = {
-				name: '',
-				hometown: '',
-				bio: '',
-				genres: '',
+        function didInitialize() {
+            //We need default values so the templating does not fail.
+            var view = this,
+                user;
+
+            user = {
+                name: '',
+                hometown: '',
+                bio: '',
+                genres: '',
                 image_url:''
-			};
+            };
             this.user = App.user;
+            _.extend(user, App.user.data);
+            this.templateParameters = user;
 
-			_.extend(user, App.user.data);
-			
-			this.templateParameters = user;
+            //Start loading profile image
+            this.profileImg = new Image();
+            this.profileImg.onload = function() {
+                view.profileImgLoaded.resolve();
+            };
+            this.profileImg.src = this.user.data.image_url;
+        }
 
-            //Image handling
-            var isVertical;
-            var img = new Image();
-            img.src = this.user.data.image_url;
-            var imgWidth = img.width;
-            var imgHeight = img.height;
-            isVertical = imgWidth < imgHeight;
+        function didRender() {
+            var view = this;
 
-            //TODO background-image is not loaded
-            var picture_url = 'url("'+this.user.data.image_url+'")';
-            console.log(picture_url);
-            $('#prof-pic-div').css("background-image",picture_url);
-            if (isVertical) {
-                $('#prof-pic-div').css("background-size","auto "+imgWidth);
-            }else{
-                $('#prof-pic-div').css("background-size",imgHeight+ "auto");
-            }
+            $.when(this.profileImgLoaded).then(function() {
+                var $profilePic = $('#prof-pic-div', view.$element),
+                    img = view.profileImg;
+                $profilePic.css('background-image', 'url("' + img.src + '")');
+                if(img.width < img.height) {
+                    $profilePic.css('background-size', 'auto ' + img.width);
+                }
+                else{
+                    $profilePic.css('background-size', img.height + ' auto');
+                }
+            });
 
+            this.setupEvent('change', '#profile-pic', this, this.handleImageUpload);
         }
 
         function handleImageUpload(event) {
