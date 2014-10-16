74c7c5f3b0153b3f65a70e0441d17c865fad6065
horatiu665 <croman12@student.aau.dk>
2014-10-16 11:41:01 +0200

    Added save button disable/enable functionality and "success" message for the user.

--- app/js/viewcontrollers/dashboard-profile.js
+++ app/js/viewcontrollers/dashboard-profile.js
@@ -1,6 +1,6 @@
 /**
  * Controller for the Sharingear Profile dashboard page view.
- * @author: Chris Hjorth
+ * @author: Chris Hjorth, Horatiu Roman
  */
 
 define(
@@ -16,6 +16,7 @@ define(
             handleImageUpload: handleImageUpload,
             didRender: didRender,
             handleSave:handleSave,
+            enableSaveButton:enableSaveButton,
 		}); 
 		return Profile;
 
@@ -51,6 +52,13 @@ define(
             $('#dashboard-profile-form #name',this.$element).val(userData.name);
             $('#dashboard-profile-form #surname',this.$element).val(userData.surname);
             $('#dashboard-profile-form #city',this.$element).val(userData.city);
+
+            // when page loads, save is disabled
+            view.enableSaveButton(false);
+            // enable save when something changes in one of the input fields
+            $('input, textarea').on('input', function() {
+                view.enableSaveButton(true);
+            });
 
             $.when(this.profileImgLoaded).then(function() {
                 var $profilePic = $('#prof-pic-div', view.$element),
@@ -94,7 +102,11 @@ define(
                 city: $('#dashboard-profile-form #hometown').val(),
                 bio: $('#dashboard-profile-form #bio').val()
             }
-            console.log('User id: ' + App.user.data.id);
+            console.log('Saved. User id: ' + App.user.data.id);
+            // if no error, show message to user, next to save button
+            $('#saveSuccessDiv').html("Your profile has been updated");
+            view.enableSaveButton(false);
+
             view.user.updateUser(App.user.data.id, saveData, function (error, data) {
                 if(error){
                     console.log(error);
@@ -102,5 +114,17 @@ define(
             });
         }
 
+        // if active==true, enables save button, else disables
+        function enableSaveButton(active) {
+            if (!active) {
+                // disable button
+                $('#saveButton').attr({disabled: "disabled"});
+            } else {
+                // enable button
+                $('#saveButton').removeAttr("disabled");
+                // clear success message
+                $('#saveSuccessDiv').html("");
+            }
+        }
 	}
 );
