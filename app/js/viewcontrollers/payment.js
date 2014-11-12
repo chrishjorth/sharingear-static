a6d3b5504984b9fb4b3c7cd360e22126c1eb8959
Chris Hjorth <contact@chrishjorth.com>
2014-11-12 15:39:26 +0100

    Added missing data fields to payment.

--- app/js/viewcontrollers/payment.js
+++ app/js/viewcontrollers/payment.js
@@ -10,6 +10,8 @@ define(
 	function(ViewController, App) {
 		var didInitialize,
 			didRender,
+			renderMissingDataInputs,
+
 			handleCancel,
 			handlePay;
 
@@ -23,8 +25,45 @@ define(
 
 		didRender = function () {
 			//the pay event must also create booking!!!
+			if(App.user.hasWallet === false) {
+				this.renderMissingDataInputs();
+			}
 			this.setupEvent('click', '#payment-cancel-btn', this, this.handleCancel);
 			this.setupEvent('submit', '#payment-form', this, this.handlePay);
+		};
+
+		renderMissingDataInputs = function() {
+			var user = App.user.data;
+			if(user.birthdate && user.birthdate !== '') {
+				$('#payment-birthdate', this.$element).parent().addClass('hidden');
+			}
+			if(user.address && user.address !== '') {
+				$('#payment-address', this.$element).parent().addClass('hidden');
+			}
+			if(user.postal_code && user.postal_code !== '') {
+				$('#payment-postalcode', this.$element).parent().addClass('hidden');
+			}
+			if(user.city && user.city !== '') {
+				$('#payment-city', this.$element).parent().addClass('hidden');
+			}
+			if(user.region && user.region !== '') {
+				$('#payment-region', this.$element).parent().addClass('hidden');
+			}
+			if(user.country && user.country !== '') {
+				$('#payment-country', this.$element).parent().addClass('hidden');
+			}
+			else {
+				populateCountries($('#payment-country', this.$element));
+			}
+			if(user.nationality && user.nationality !== '') {
+				$('#payment-nationality', this.$element).parent().addClass('hidden');
+			}
+			else {
+				populateCountries($('#payment-nationality', this.$element));
+			}
+			if(user.phone && user.phone !== '') {
+				$('#payment-phone', this.$element).parent().addClass('hidden');
+			}
 		};
 
 		handleCancel = function() {
@@ -47,6 +86,8 @@ define(
 
 			didInitialize: didInitialize,
 			didRender: didRender,
+			renderMissingDataInputs: renderMissingDataInputs,
+
 			handleCancel: handleCancel,
 			handlePay: handlePay
 		});
