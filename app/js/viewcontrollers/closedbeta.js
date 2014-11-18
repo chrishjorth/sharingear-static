01df25e1f1a25d612f8b55a5375a398975dcfd73
Chris Hjorth <contact@chrishjorth.com>
2014-11-18 15:03:51 +0100

    Added closed beta lockout.

--- app/js/viewcontrollers/closedbeta.js
+++ app/js/viewcontrollers/closedbeta.js
@@ -1,32 +1,53 @@
 /**
  * Controller for the Sharingear Closed Beta page view.
+ * Users log in with their Facebook account, then we check their fb id or email against our list of allowed users.
  * @author: Chris Hjorth
  */
 
 'use strict';
 
 define(
-	['viewcontroller', 'app'],
-	function(ViewController, App) {
-		var didRender,
+	['jquery', 'viewcontroller', 'app'],
+	function($, ViewController, App) {
+		var didInitialize,
+			didRender,
 
-			handleContinue;
+			handleLoginToBeta;
+
+		didInitialize = function() {
+			
+		};
 
 		didRender = function() {
-			var $ctoEmail = $('#closedbeta-ctomail', this.$element);
+			var $ctoEmail = $('.ctomail', this.$element);
 			$ctoEmail.attr('href', 'mailto:chris' + '@sharin'+ 'gear.com');
 			$ctoEmail.html('chris' + '@sharin'+ 'gear.com');
 
-			this.setupEvent('click', '#closedbeta-continue-btn', this, this.handleContinue);
+			this.setupEvent('click', '#closedbeta-login-btn', this, this.handleLoginToBeta);
 		};
 
-		handleContinue = function() {
-			App.router.closeModalView();
+		handleLoginToBeta = function(event) {
+			var view = event.data;
+			App.user.login(function(error) {
+				if(error) {
+					console.log(error);
+					alert('Error loggin in.');
+					return;
+				}
+				if(App.user.data.id === null) {
+					$('#closedbeta-intro', view.$element).addClass('hidden');
+					$('#closedbeta-noaccess', view.$element).removeClass('hidden');
+				}
+				else {
+					App.router.closeModalView();
+				}
+			});
 		};
 
 		return ViewController.inherit({
+			didInitialize: didInitialize,
 			didRender: didRender,
-			handleContinue: handleContinue
+			handleLoginToBeta: handleLoginToBeta
 		});
 	}
 );
