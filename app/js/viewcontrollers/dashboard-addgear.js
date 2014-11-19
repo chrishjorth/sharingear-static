52da99949aa6423171aad056383ac41fe75412b9
Chris Hjorth <contact@chrishjorth.com>
2014-11-19 11:41:50 +0100

    Converted addgear to strict and added validation.

--- app/js/viewcontrollers/dashboard-addgear.js
+++ app/js/viewcontrollers/dashboard-addgear.js
@@ -3,31 +3,35 @@
  * @author: Chris Hjorth, Horatiu Roman
  */
 
+'use strict';
+
 define(
-	['underscore', 'viewcontroller', 'app', 'models/gear'],
-	function(_, ViewController, App, Gear) {
-		var AddGear = ViewController.inherit({
-			newGear: null,
+	['underscore', 'jquery', 'viewcontroller', 'app', 'models/gear'],
+	function(_, $, ViewController, App, Gear) {
+		var subtypeDefault = 'Choose subtype:',
+			brandDefault = 'Choose brand:',
+			newGear,
 
-			didInitialize: didInitialize,
-			didRender: didRender,
-			addGearIcons: addGearIcons,
-			handleGearRadio: handleGearRadio,
-			handleSelectSubtype: handleSelectSubtype,
-			handleNext: handleNext,
-			populateSubtypeSelect: populateSubtypeSelect,
-			populateBrandSelect: populateBrandSelect,
-			prepopulate: prepopulate
-		}); 
-		return AddGear;
+			didInitialize,
+			didRender,
 
-		function didInitialize() {
+			addGearIcons,
+			populateSubtypeSelect,
+			populateBrandSelect,
+			handleGearRadio,
+			handleSelectSubtype,
+			handleNext,
+			prepopulate;
+
+		newGear = null;
+
+		didInitialize = function() {
 			if(App.user.data.id === null) {
 				this.ready = false;
 				App.router.navigateTo('home');
 				return;
 			}
-			this.newGear = new Gear.constructor({
+			newGear = new Gear.constructor({
 				rootURL: App.API_URL,
 				data: {
 					id: null,
@@ -36,32 +40,27 @@ define(
 			});
 
 			if(this.passedData) {
-				_.extend(this.newGear.data, this.passedData.data);
+				_.extend(newGear.data, this.passedData.data);
 			}
-		}
+		};
 
-		function didRender() {
+		didRender = function() {
 			var view = this;
 			this.addGearIcons(function() {
 				view.prepopulate();
 			});
 
 			this.setupEvent('submit', '#dashboard-addgear-form', this, this.handleNext);
-		}
+		};
 
-		function addGearIcons(callback) {
+		addGearIcons = function(callback) {
 			var view = this;
 			App.gearClassification.getClassification(function(gearClassification) {
-				var $gearbuttonlistContainer = $('.gearbuttonlist-container', this.$element),
+				var $gearbuttonlistContainer = $('.gearbuttonlist-container', view.$element),
 					html = '',
 					gearType;
 
 				for(gearType in gearClassification.classification) {
-				//	html += '<div class="custom-radio">
-				//				<input type="radio" class="test" name="gear-radio" id="gear-radio-' + gearType + '" value="' + gearType + '">
-				//				<label for="gear-radio-' + gearType + '"><img src="images/addgear/addgear_' + gearType.toLowerCase() + '.png">
-				//				</label>
-				//			</div>';
 					html += '<div class="custom-radio">';
 					html += '<input type="radio" name="gear-radio" id="gear-radio-' + gearType + '" value="' + gearType + '">';
 					html += '<label for="gear-radio-' + gearType + '">';
@@ -72,24 +71,17 @@ define(
 
 				}
 
-//		        <div class="custom-checkbox">
- //               <input type="checkbox" id="custom-checkbox-guitar" name="guitar" value="guitar">
-   //             <label for="custom-checkbox-guitar">
-     //           <img src="images/instrumenticon_gtr.png" class="custom-checkbox-image">Guitar</label>
-       //       </div>
-
-
 				$gearbuttonlistContainer.append(html);
 				view.setupEvent('change', '#dashboard-addgear-form .gearbuttonlist-container input[type="radio"]', view, view.handleGearRadio);
 				if(callback && typeof callback === 'function') {
 					callback();
 				}
 			});
-		}
+		};
 
-		function populateSubtypeSelect(gearType) {
+		populateSubtypeSelect = function(gearType) {
 			var gearClassification = App.gearClassification.data.classification,
-				html = '<option> Choose type: </option>',
+				html = '<option> ' + subtypeDefault + ' </option>',
 				$subtypeSelect, $brandSelectContainer, gearSubtypes, i;
 
 			$('#gear-subtype-container', this.$element).removeClass('hidden');
@@ -108,11 +100,11 @@ define(
 			}
 			$subtypeSelect.append(html);
 			this.setupEvent('change', '#gear-subtype-container select', this, this.handleSelectSubtype);
-		}
+		};
 
-		function populateBrandSelect() {
+		populateBrandSelect = function() {
 			var brands = App.gearClassification.data.brands,
-				html = '<option> Choose brand: </option>',
+				html = '<option> ' + brandDefault + ' </option>',
 				$brandSelect, i;
 
 			$('#gear-brand-container', this.$element).removeClass('hidden');
@@ -124,24 +116,23 @@ define(
 				html += '<option value="' + brands[i] + '">' + brands[i] + '</option>';
 			}
 			$brandSelect.append(html);
-		}
+		};
 
 		/**
 		 * @assertion: gearClassification has been loaded
 		 */
-		function handleGearRadio(event) {
+		handleGearRadio = function(event) {
 			var view = event.data;
 			view.populateSubtypeSelect($(this).val());
-		}
+		};
 
-		function handleSelectSubtype(event) {
+		handleSelectSubtype = function(event) {
 			var view = event.data;
 			view.populateBrandSelect();
-		}
+		};
 
-		function handleNext(event) {
-			var view = event.data,
-				newData, callback;
+		handleNext = function() {
+			var newData, callback;
 
 			//Create new gear model object from form data
 			newData = {
@@ -152,47 +143,77 @@ define(
 				description: $('#dashboard-addgear-form-description').val()
 			};
 
-			_.extend(view.newGear.data, newData);
+			//Validate
+			if(!newData.type || newData.type === '') {
+				alert('Please select a type of instrument.');
+				return;
+			}
+			if(newData.subtype === '' || newData.subtype === subtypeDefault) {
+				alert('Please select a subtype for your instrument.');
+				return;
+			}
+			if(newData.brand === '' || newData.brand === brandDefault) {
+				alert('Please select the instrument\'s brand.');
+				return;
+			}
+			if(newData.model === '') {
+				alert('Please type in the model of your instrument.');
+				return;
+			}
 
-			callback = function(error, data) {
+			_.extend(newGear.data, newData);
+
+			callback = function(error) {
 				if(error) {
 					alert('Error saving gear');
 					return;
 				}
-				App.router.navigateTo('dashboard/addgearphotos', view.newGear);
-			}
+				App.router.navigateTo('dashboard/addgearphotos', newGear);
+			};
 
-			if(view.newGear.data.id === null) {
-				view.newGear.createGear(App.user, callback);
+			if(newGear.data.id === null) {
+				newGear.createGear(App.user, callback);
 			}
 			else {
 				//Case of the user breadcrumbing back
-				view.newGear.save(App.user.data.id, callback);
+				newGear.save(App.user.data.id, callback);
 			}
-		}
+		};
 
 		/**
 		 * Prefills the form with passed data.
 		 */
-		function prepopulate() {
-			var newGear;
+		prepopulate = function() {
+			var gear;
 			if(this.passedData === null || !(this.passedData)) { // added !(this.passedData) because it was undefined. Horatiu
 				return;
 			}
-			newGear = this.passedData.data;
-			if(newGear.type && newGear.type.length >= 0) {
-				$('#dashboard-addgear-form .gearbuttonlist-container #gear-radio-' + newGear.type.toLowerCase() + '').prop("checked", true);
-				this.populateSubtypeSelect(newGear.type);
-				if(newGear.subtype && newGear.subtype.length >= 0) {
-					$('#gear-subtype-container select').val(newGear.subtype);
+			gear = this.passedData.data;
+			if(gear.type && gear.type.length >= 0) {
+				$('#dashboard-addgear-form .gearbuttonlist-container #gear-radio-' + gear.type.toLowerCase() + '').prop('checked', true);
+				this.populateSubtypeSelect(gear.type);
+				if(gear.subtype && gear.subtype.length >= 0) {
+					$('#gear-subtype-container select').val(gear.subtype);
 					this.populateBrandSelect();
-					if(newGear.brand && newGear.brand.length >= 0) {
-						$('#gear-brand-container select').val(newGear.brand);
+					if(gear.brand && gear.brand.length >= 0) {
+						$('#gear-brand-container select').val(gear.brand);
 					}
 				}
 			}
-			$('#dashboard-addgear-form-model').val(newGear.model);
-			$('#dashboard-addgear-form-description').val(newGear.description);
-		}
+			$('#dashboard-addgear-form-model').val(gear.model);
+			$('#dashboard-addgear-form-description').val(gear.description);
+		};
+
+		return ViewController.inherit({
+			didInitialize: didInitialize,
+			didRender: didRender,
+			addGearIcons: addGearIcons,
+			handleGearRadio: handleGearRadio,
+			handleSelectSubtype: handleSelectSubtype,
+			handleNext: handleNext,
+			populateSubtypeSelect: populateSubtypeSelect,
+			populateBrandSelect: populateBrandSelect,
+			prepopulate: prepopulate
+		});
 	}
 );
