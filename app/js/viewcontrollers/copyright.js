66d3ce005d6d7a54cfede7df534b213bbfb3c85b
Dimitar Gyurov <dgyurov@gmail.com>
2014-11-28 17:46:37 +0100

    Solved issue #164.

---
+++ app/js/viewcontrollers/copyright.js
@@ -0,0 +1,12 @@
+/**
+ * Controller for the Sharingear Insurance page view.
+ * @author: Chris Hjorth
+ */
+
+define(
+	['viewcontroller'],
+	function(ViewController) {
+		var Copyright = ViewController;
+		return Copyright;
+	}
+);
