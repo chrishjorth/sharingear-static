19899d8883c6e381bf94852a0d7c82aa40fc21c1
Dimitar Gyurov <dgyurov@gmail.com>
2014-12-18 10:05:42 +0100

    Solved issue #219.

--- app/js/viewcontrollers/aboutus.js
+++ app/js/viewcontrollers/aboutus.js
@@ -6,8 +6,41 @@
 'use strict';
 
 define(
-	['viewcontroller'],
-	function(ViewController) {
-		return ViewController.inherit({});
+	['viewcontroller','googlemaps'],
+	function(ViewController, GoogleMaps) {
+		var didInitialize,
+			didRender,
+			renderMap;
+
+		didInitialize = function(){
+
+		};
+
+		didRender = function () {
+			this.renderMap();
+		};
+
+		renderMap = function() {
+			var mapOptions, latlong, marker;
+			latlong = new GoogleMaps.LatLng(55.682276, 12.577168);
+			mapOptions = {
+				center: latlong,
+				zoom: 14,
+				maxZoom: 14
+			};
+			this.map = new GoogleMaps.Map(document.getElementById('aboutus-map'), mapOptions);
+			marker = new GoogleMaps.Marker({
+				position: latlong,
+				map: this.map,
+				icon: 'images/shagicon_003.png' // TODO: put icon on server
+			});
+		};
+
+	return ViewController.inherit({
+		didInitialize: didInitialize,
+		didRender: didRender,
+		renderMap: renderMap
+	});
+
 	}
 );
