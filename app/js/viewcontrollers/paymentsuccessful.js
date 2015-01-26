8400c4caae73dfe88ec44f88504ceb99ecf2eef3
Chris Hjorth <contact@chrishjorth.com>
2015-01-26 17:05:23 +0100

    Payment successful polish.

--- app/js/viewcontrollers/paymentsuccessful.js
+++ app/js/viewcontrollers/paymentsuccessful.js
@@ -6,26 +6,35 @@
 'use strict';
 
 define(
-	['jquery', 'viewcontroller', 'app', 'models/booking', 'moment'],
-	function($, ViewController, App, Booking, Moment) {
-		var booking,
-
-			didInitialize,
+	['jquery', 'viewcontroller', 'app', 'models/booking', 'moment', 'models/localization'],
+	function($, ViewController, App, Booking, Moment, Localization) {
+		var didInitialize,
 			didRender,
 
 			handleClose;
 
 		didInitialize = function () {
-			var view = this;
+			var view = this,
+				booking;
 
 			view.paymentSuccessful = null; //null: waiting for server
 
 			view.templateParameters = {
-				start_time: '',
-				end_time: '',
+				brand: '',
+				subtype: '',
+				model: '',
+				start_date: '',
+				end_date: '',
+				currency: 'DKK',
+				vat: '',
 				price: '',
-				currency: 'DKK'
+				price_vat: '',
+				fee: '',
+				fee_vat: '',
+				total: ''
 			};
+
+			this.gear = this.passedData.gear;
 
 			booking = new Booking.constructor({
 				rootURL: App.API_URL
@@ -34,7 +43,8 @@ define(
 			booking.data.id = this.passedData.bookingID;
 			booking.data.preauth_id = this.passedData.preAuthorizationID;
 			booking.data.booking_status = 'pending';
-			booking.data.gear_id = this.passedData.gear_id;
+			booking.data.gear_id = this.gear.data.id;
+
 			booking.update(App.user.data.id, function(error) {
 				if(error) {
 					console.log('Error updating booking: ' + error);
@@ -44,19 +54,45 @@ define(
 				}
 
 				booking.getBookingInfo(App.user.data.id, function(error){
-					var timeFormat, formattedEnd, formattedStart;
+					var startMoment, endMoment, duration, months, weeks, days, price, VAT, priceVAT, fee, feeVAT;
 					if(error) {
 						console.log('Error getting booking info: ' + error);
 						return;
 					}
 
-					timeFormat = 'MMMM Do YYYY, H:mm';
-					formattedEnd = new Moment(booking.data.end_time).format(timeFormat);
-					formattedStart = new Moment(booking.data.start_time).format(timeFormat);
+					startMoment = new Moment(booking.data.start_time, 'YYYY-MM-DD HH:mm:ss');
+					endMoment = new Moment(booking.data.end_time, 'YYYY-MM-DD HH:mm:ss');
 
-					view.templateParameters.start_time = formattedStart;
-					view.templateParameters.end_time = formattedEnd;
-					view.templateParameters.price = booking.data.price;
+					duration = Moment.duration(endMoment.diff(startMoment));
+					months = parseInt(duration.months(), 10);
+					endMoment.subtract(months, 'months');
+					duration = Moment.duration(endMoment.diff(startMoment));
+					weeks = parseInt(duration.weeks(), 10);
+					endMoment.subtract(weeks, 'weeks');
+					duration = Moment.duration(endMoment.diff(startMoment));
+					days = parseInt(duration.days(), 10);
+
+					price = booking.data.price;
+					VAT = Localization.getVAT(App.user.data.country);
+					priceVAT = price / 100 * VAT;
+					fee = price / 100 * App.user.data.buyer_fee;
+					feeVAT = fee / 100 * VAT;
+
+					view.templateParameters = {
+						brand: view.gear.data.brand,
+						subtype: view.gear.data.subtype,
+						model: view.gear.data.model,
+						start_date: startMoment.format('DD/MM/YYYY'),
+						end_date: endMoment.format('DD/MM/YYYY'),
+						currency: 'DKK',
+						vat: VAT,
+						price: price,
+						price_vat: priceVAT,
+						fee: fee,
+						fee_vat: feeVAT,
+						total: price + priceVAT + fee + feeVAT
+					};
+
 					view.paymentSuccessful = true;
 					view.render();
 				});
@@ -70,7 +106,7 @@ define(
 			if(this.paymentSuccessful === false) {
 				$('.payment-failure', this.$element).removeClass('hidden');
 			}
-			this.setupEvent('click', '#paymentsuccess-close-btn', this, this.handleClose);
+			this.setupEvent('click', '#paymentsuccessful-close-btn', this, this.handleClose);
 		};
 
 		handleClose = function() {
