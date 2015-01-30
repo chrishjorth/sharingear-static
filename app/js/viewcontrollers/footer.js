e3df1621f67a231284074672ef0bb75c88b378f7
Chris Hjorth <contact@chrishjorth.com>
2015-01-30 16:31:40 +0100

    Home/landing view polish + footer polish.

--- app/js/viewcontrollers/footer.js
+++ app/js/viewcontrollers/footer.js
@@ -3,10 +3,23 @@
  * @author: Chris Hjorth
  */
 
+'use strict';
+
 define(
-	['viewcontroller'],
-	function(ViewController) {
-		var Footer = ViewController.inherit();
-		return Footer;
+	['viewcontroller', 'moment'],
+	function(ViewController, Moment) {
+		var didInitialize;
+
+		didInitialize = function() {
+			var today = new Moment();
+			
+			this.templateParameters = {
+				year: today.format('YYYY')
+			};
+		};
+
+		return ViewController.inherit({
+			didInitialize: didInitialize
+		});
 	}
 );
