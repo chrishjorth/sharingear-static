9b595034050fad65a1e3d04000581328edef20cd
Chris Hjorth <contact@chrishjorth.com>
2015-02-03 20:41:00 +0100

    cleanup.

--- app/js/router.js
+++ app/js/router.js
@@ -84,7 +84,6 @@ define(
 						newLocation += '?' + queryString;
 					}
 					newLocation += '#' + route;
-					console.log('route debug: ' + newLocation);
 					history.replaceState({}, '', newLocation); //This is to avoid calling handleHashChange by setting window.location.hash directly
 				}
 			}
