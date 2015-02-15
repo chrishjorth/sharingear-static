2e0bea16215740ed828d5ed23f78a05a0b22eee6
Chris Hjorth <contact@chrishjorth.com>
2015-02-15 15:49:11 +0100

    Added save time zone setting.

--- app/js/viewcontrollers/dashboard-settings.js
+++ app/js/viewcontrollers/dashboard-settings.js
@@ -8,9 +8,23 @@
 define(
 	['jquery', 'viewcontroller', 'app', 'models/localization'],
 	function($, ViewController, App, Localization) {
-		var didRender;
+		var didInitialize,
+			didRender,
+			populateTimeZones,
+
+			handleSave;
+
+		didInitialize = function() {
+			this.selectedTimeZone = App.user.data.time_zone;
+		};
 
 		didRender = function() {
+			this.populateTimeZones();
+
+			this.setupEvent('submit', '#dashboard-settings-form', this, this.handleSave);
+		};
+
+		populateTimeZones = function() {
 			var timezones = Localization.getTimeZones(),
 				$timezonesSelect = $('#dashboard-settings-timezone', this.$element),
 				html = '',
@@ -25,12 +39,44 @@ define(
 			}
 			$timezonesSelect.html(html);
 
-			defaultTimezone = App.user.data.time_zone;
+			defaultTimezone = this.selectedTimeZone;
+			if(defaultTimezone === null) {
+				defaultTimezone = 'UTC';
+			}
 			$timezonesSelect.val(defaultTimezone);
+		};
+
+		handleSave = function(event) {
+			var view = event.data,
+				selectedTimeZone, $successMessage, $saveBtn;
+			
+			selectedTimeZone = $('#dashboard-settings-timezone', view.$element).val();
+			$successMessage = $('#dashboard-settings-savesuccess', view.$element),
+			$saveBtn = $('#dashboard-settings-savebtn', view.$element);
+
+			$successMessage.addClass('hidden');
+			$saveBtn.html('<i class="fa fa-circle-o-notch fa-fw fa-spin">');
+
+			if(selectedTimeZone !== view.selectedTimeZone) {
+				App.user.data.time_zone = selectedTimeZone;
+				App.user.update(function(error) {
+					if(error) {
+						alert('Error saving settings.');
+						console.log('Error saving settings: ' + error);
+						return;
+					}
+					$successMessage.removeClass('hidden');
+					$saveBtn.html('Save');
+				});
+			}
 		};
 		
 		return ViewController.inherit({
-			didRender: didRender
+			didInitialize: didInitialize,
+			didRender: didRender,
+			populateTimeZones: populateTimeZones,
+
+			handleSave: handleSave
 		});
 	}
 );
