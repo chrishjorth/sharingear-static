6d29fdf353fe377982ed7a7a3756c6f40fdacb82
Chris Hjorth <contact@chrishjorth.com>
2015-03-09 20:59:50 +0100

    Fixed price calculation bug in payment view.

--- app/js/viewcontrollers/payment.js
+++ app/js/viewcontrollers/payment.js
@@ -64,8 +64,8 @@ define(
 			};
 
 			this.isPaying = false;
-
-			Localization.convertPrices([this.booking.data.price_a, this.booking.data.price_b, this.booking.data.price_c], this.owner.data.currency, App.user.data.currency, function(error, convertedPrices, rate) {
+			
+			Localization.convertPrices([this.booking.data.price_a, this.booking.data.price_b, this.booking.data.price_c], this.booking.data.currency, App.user.data.currency, function(error, convertedPrices, rate) {
 				var price;
 				if(error) {
 					console.log('Error converting prices: ' + error);
