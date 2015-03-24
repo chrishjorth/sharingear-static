b1615dbd8e70c68a0defb13f459bef336767f02e
Chris Hjorth <contact@chrishjorth.com>
2015-03-24 14:06:04 +0100

    Switched to production environment.

--- app/js/bundle.js
+++ app/js/bundle.js
@@ -11166,7 +11166,7 @@
 	/*jslint node: true */
 	'use strict';
 	
-	var IS_PRODUCTION = false, //This variable should be set and saved according to the git branch: true for master and false for develop
+	var IS_PRODUCTION = true, //This variable should be set and saved according to the git branch: true for master and false for develop
 	    MIN_USER_AGE = 13,
 	    AVG_USER_AGE = 27,
 	    MIN_XP_START_YEAR = 1960,
