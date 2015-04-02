4dfeeaf2e5d42442fdc0dcbb34bdaf1dc028d62d
Chris Hjorth <contact@chrishjorth.com>
2015-04-02 15:34:03 +0200

    Updated node modules. Moved testimonials from about us to home page.

--- app/js/viewcontrollers/aboutus.js
+++ app/js/viewcontrollers/aboutus.js
@@ -6,13 +6,10 @@
 /*jslint node: true */
 'use strict';
 
-var _ = require('underscore'),
-    $ = require('jquery'),
+var $ = require('jquery'),
     GoogleMaps = require('../libraries/mscl-googlemaps.js'),
 
     ViewController = require('../viewcontroller'),
-
-    testimonials,
 
     didInitialize,
     didRender,
@@ -21,78 +18,9 @@ var _ = require('underscore'),
     renderMap,
     loadFooter;
 
-testimonials = [{
-    image_file: 'images/testimonials/3.jpg',
-    citation: 'Got some gear or need some gear? It\'s all there on Sharingear. Get your gear out there, and get it workin\' for you.',
-    name: 'stephen carpenter',
-    role: 'guitarist',
-    band: 'deftones'
-}, {
-    image_file: 'images/testimonials/1.jpg',
-    citation: 'It\'s kind of like a marketplace, friendship and community to share instruments and save costs and make some money. It\'s a way to make it easier for each other as musicians in bands - a way for everyone to help out.',
-    name: 'peter dolving',
-    role: 'vocalist',
-    band: 'ex-the haunted/iamfire'
-}, {
-    image_file: 'images/testimonials/2.jpg',
-    citation: 'If you are in a band, flying into some place in Europe, sometimes instead of having to get the whole backline, maybe if you just need to fly in and play one show, there are musicians in other towns, possibliy in your town, that have gear just laying around you can rent.',
-    name: 'ryan knight',
-    role: 'guitarist',
-    band: 'the black dahlia murder'
-}, {
-    image_file: 'images/testimonials/4.jpg',
-    citation: 'This is an extremely cool service and something that every musician needs to use. From time to time I do rent out of my mesa-boogie cabinets, and with this platform I am able to connect with more musicians than before. Try it out, I guarantee its a great experience for everyone playing music.',
-    name: 'franz gottschalk',
-    role: 'guitarist',
-    band: 'ex-volbeat'
-}, {
-    image_file: 'images/testimonials/6.jpg',
-    citation: 'Musicians need to work more close together as the market doesn\'t leave anything left for those who actually create the music. Sharingear facilitates a networking opportunity, where you can not only save costs but also make cash from your gear.',
-    name: 'ken holst',
-    role: 'guitarist',
-    band: 'illdisposed'
-}, {
-    image_file: 'images/testimonials/5.jpg',
-    citation: 'Need som equipment for a recording session and don\'t think your own gear is up to par? Or do you just want to try out some different possibilities in your own rehearsal space before deciding what to buy for yourself? Sharingear is the way to go...',
-    name: 'mathias jensen',
-    role: 'guitarist',
-    band: 'hobby musician'
-}];
-
-didInitialize = function() {
-
-};
-
 didRender = function() {
-    this.renderTestimonials();
     this.renderMap();
     this.loadFooter();
-};
-
-renderTestimonials = function() {
-    var view = this,
-        TestimonialTemplate;
-
-    TestimonialTemplate = require('../../templates/testimonial.html');
-
-    var testimonialTemplate = _.template(TestimonialTemplate),
-        $owlContainer = $('.owl-carousel', view.$element),
-        $testimonial,
-        i;
-
-    for (i = 0; i < testimonials.length; i++) {
-        $testimonial = $(testimonialTemplate(testimonials[i]));
-        $('.profile-pic', $testimonial).css({
-            'background-image': 'url("' + testimonials[i].image_file + '")'
-        });
-        $owlContainer.append($testimonial);
-    }
-
-    $owlContainer.owlCarousel({
-        slideSpeed: 300,
-        paginationSpeed: 400,
-        singleItem: true
-    });
 };
 
 renderMap = function() {
--- app/js/viewcontrollers/home.js
+++ app/js/viewcontrollers/home.js
@@ -6,20 +6,62 @@
 /*jslint node: true */
 'use strict';
 
-var $ = require('jquery'),
+var _ = require('underscore'),
+    $ = require('jquery'),
 
     ViewController = require('../viewcontroller.js'),
     App = require('../app.js'),
+
+    testimonials,
 
     didInitialize,
     didRender,
 
     loadSearchBar,
+    renderTestimonials,
     loadFooter,
 
     handleTab,
     handleLogin,
     handleScrollDown;
+
+testimonials = [{
+    image_file: 'images/testimonials/3.jpg',
+    citation: 'Got some gear or need some gear? It\'s all there on Sharingear. Get your gear out there, and get it workin\' for you.',
+    name: 'stephen carpenter',
+    role: 'guitarist',
+    band: 'deftones'
+}, {
+    image_file: 'images/testimonials/1.jpg',
+    citation: 'It\'s kind of like a marketplace, friendship and community to share instruments and save costs and make some money. It\'s a way to make it easier for each other as musicians in bands - a way for everyone to help out.',
+    name: 'peter dolving',
+    role: 'vocalist',
+    band: 'ex-the haunted/iamfire'
+}, {
+    image_file: 'images/testimonials/2.jpg',
+    citation: 'If you are in a band, flying into some place in Europe, sometimes instead of having to get the whole backline, maybe if you just need to fly in and play one show, there are musicians in other towns, possibliy in your town, that have gear just laying around you can rent.',
+    name: 'ryan knight',
+    role: 'guitarist',
+    band: 'the black dahlia murder'
+}, {
+    image_file: 'images/testimonials/4.jpg',
+    citation: 'This is an extremely cool service and something that every musician needs to use. From time to time I do rent out of my mesa-boogie cabinets, and with this platform I am able to connect with more musicians than before. Try it out, I guarantee its a great experience for everyone playing music.',
+    name: 'franz gottschalk',
+    role: 'guitarist',
+    band: 'ex-volbeat'
+}, {
+    image_file: 'images/testimonials/6.jpg',
+    citation: 'Musicians need to work more close together as the market doesn\'t leave anything left for those who actually create the music. Sharingear facilitates a networking opportunity, where you can not only save costs but also make cash from your gear.',
+    name: 'ken holst',
+    role: 'guitarist',
+    band: 'illdisposed'
+}, {
+    image_file: 'images/testimonials/5.jpg',
+    citation: 'Need som equipment for a recording session and don\'t think your own gear is up to par? Or do you just want to try out some different possibilities in your own rehearsal space before deciding what to buy for yourself? Sharingear is the way to go...',
+    name: 'mathias jensen',
+    role: 'guitarist',
+    band: 'hobby musician'
+}];
 
 didInitialize = function() {
     this.hasSubviews = true;
@@ -33,6 +75,7 @@ didRender = function() {
     }
 
     this.loadSearchBar();
+    this.renderTestimonials();
     this.loadFooter();
 
     this.setupEvent('click', '.sg-tabbar li .sg-btn-square', this, this.handleTab);
@@ -71,6 +114,32 @@ loadSearchBar = function() {
     });
     view.vanSearchFormVC.initialize();
     view.vanSearchFormVC.render();
+};
+
+renderTestimonials = function() {
+    var view = this,
+        TestimonialTemplate;
+
+    TestimonialTemplate = require('../../templates/testimonial.html');
+
+    var testimonialTemplate = _.template(TestimonialTemplate),
+        $owlContainer = $('.owl-carousel', view.$element),
+        $testimonial,
+        i;
+
+    for (i = 0; i < testimonials.length; i++) {
+        $testimonial = $(testimonialTemplate(testimonials[i]));
+        $('.profile-pic', $testimonial).css({
+            'background-image': 'url("' + testimonials[i].image_file + '")'
+        });
+        $owlContainer.append($testimonial);
+    }
+
+    $owlContainer.owlCarousel({
+        slideSpeed: 300,
+        paginationSpeed: 400,
+        singleItem: true
+    });
 };
 
 loadFooter = function() {
@@ -129,6 +198,7 @@ module.exports = ViewController.inherit({
     didRender: didRender,
 
     loadSearchBar: loadSearchBar,
+    renderTestimonials: renderTestimonials,
     loadFooter: loadFooter,
 
     handleTab: handleTab,
