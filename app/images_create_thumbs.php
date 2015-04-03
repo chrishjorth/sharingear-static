7ec3f4d9aad3052a2a131c412cfe90c64e4d1cc6
Chris Hjorth <contact@chrishjorth.com>
2015-04-03 14:25:43 +0200

    Live dev.

--- app/images_create_thumbs.php
+++ app/images_create_thumbs.php
@@ -28,81 +28,82 @@ $list = $storage->objects->listObjects($bucket);
 
 foreach($list['items'] as $item) {
 	echo 'Working on ' . $item->name . ' <br>';
-	
-	$request = new Google_Http_Request($item->mediaLink, 'GET');
-	$signed_request = $client->getAuth()->sign($request);
-	$http_request = $client->getIo()->makeRequest($signed_request);
+	if(strpos($item->name, 'thumb') === false) {
+		$request = new Google_Http_Request($item->mediaLink, 'GET');
+		$signed_request = $client->getAuth()->sign($request);
+		$http_request = $client->getIo()->makeRequest($signed_request);
 
-	$image = new Imagick();
-	$image->readImageBlob($http_request->getResponseBody());
+		$image = new Imagick();
+		$image->readImageBlob($http_request->getResponseBody());
 
-	$width = $image->getImageWidth();
-	$height = $image->getImageHeight();
+		$width = $image->getImageWidth();
+		$height = $image->getImageHeight();
 
-	if($width >= $height) {
-		if($width > MAX_SIZE) {
-			//resize by width
-			echo 'Scale by width.<br>';
-			$image->scaleImage(MAX_SIZE, 0);
+		if($width >= $height) {
+			if($width > MAX_SIZE) {
+				//resize by width
+				echo 'Scale by width.<br>';
+				$image->scaleImage(MAX_SIZE, 0);
+			}
 		}
-	}
-	else {
-		if($height > MAX_SIZE) {
-			//resize by height
-			echo 'Scale by height.<br>';
-			$image->scaleImage(0, MAX_SIZE);
+		else {
+			if($height > MAX_SIZE) {
+				//resize by height
+				echo 'Scale by height.<br>';
+				$image->scaleImage(0, MAX_SIZE);
+			}
 		}
+
+		//Create thumbs
+		$thumb1 = clone $image;
+		$thumb2 = clone $image;
+
+		if($width >= $height) {
+			$thumb1->thumbnailImage(THUMB_SIZE, 0);
+			$thumb2->thumbnailImage(THUMB_SIZE * 2, 0);
+		}
+		else {
+			$thumb1->thumbnailImage(0, THUMB_SIZE);
+			$thumb2->thumbnailImage(0, THUMB_SIZE * 2);
+		}
+
+		echo 'Save files.<br>';
+
+		$obj = new Google_Service_Storage_StorageObject();
+		$obj->setName($item->name);
+		$storage->objects->insert(
+    		$bucket,
+    		$obj,
+    		['name' => $item->name, 'data' => $image->getImageBlob(), 'uploadType' => 'media']
+		);
+
+		$filename_components = explode('.', $item->name);
+		$filename = $filename_components[0];
+		$ext = $filename_components[1];
+
+		$obj_thumb1 = new Google_Service_Storage_StorageObject();
+		$obj_thumb1->setName($filename . '_thumb.' . $ext);
+		$storage->objects->insert(
+    		$bucket,
+    		$obj_thumb1,
+    		['name' => $filename . '_thumb.' . $ext, 'data' => $thumb1->getImageBlob(), 'uploadType' => 'media']
+		);
+
+		$obj_thumb2 = new Google_Service_Storage_StorageObject();
+		$obj_thumb2->setName($filename . '_thumb@2x.' . $ext);
+		$storage->objects->insert(
+    		$bucket,
+    		$obj_thumb2,
+    		['name' => $filename . '_thumb@2x.' . $ext, 'data' => $thumb2->getImageBlob(), 'uploadType' => 'media']
+		);
+
+		$image->clear();
+		$image->destroy();
+		$thumb1->clear();
+		$thumb1->destroy();
+		$thumb2->clear();
+		$thumb2->destroy();
+
+		echo 'Done.<br><br>';
 	}
-
-	//Create thumbs
-	$thumb1 = clone $image;
-	$thumb2 = clone $image;
-
-	if($width >= $height) {
-		$thumb1->thumbnailImage(THUMB_SIZE, 0);
-		$thumb2->thumbnailImage(THUMB_SIZE * 2, 0);
-	}
-	else {
-		$thumb1->thumbnailImage(0, THUMB_SIZE);
-		$thumb2->thumbnailImage(0, THUMB_SIZE * 2);
-	}
-
-	echo 'Save files.<br>';
-
-	$obj = new Google_Service_Storage_StorageObject();
-	$obj->setName($item->name);
-	$storage->objects->insert(
-    	$bucket,
-    	$obj,
-    	['name' => $item->name, 'data' => $image->getImageBlob(), 'uploadType' => 'media']
-	);
-
-	$filename_components = explode('.', $item->name);
-	$filename = $filename_components[0];
-	$ext = $filename_components[1];
-
-	$obj_thumb1 = new Google_Service_Storage_StorageObject();
-	$obj_thumb1->setName($filename . '_thumb.' . $ext);
-	$storage->objects->insert(
-    	$bucket,
-    	$obj_thumb1,
-    	['name' => $filename . '_thumb.' . $ext, 'data' => $thumb1->getImageBlob(), 'uploadType' => 'media']
-	);
-
-	$obj_thumb2 = new Google_Service_Storage_StorageObject();
-	$obj_thumb2->setName($filename . '_thumb@2x.' . $ext);
-	$storage->objects->insert(
-    	$bucket,
-    	$obj_thumb2,
-    	['name' => $filename . '_thumb@2x.' . $ext, 'data' => $thumb2->getImageBlob(), 'uploadType' => 'media']
-	);
-
-	$image->clear();
-	$image->destroy();
-	$thumb1->clear();
-	$thumb1->destroy();
-	$thumb2->clear();
-	$thumb2->destroy();
-
-	echo 'Done.<br><br>';
 }
